ef51fedbb6193082862959709a34aa766155345f
Charley Gros <charleygros>
2021-05-25 02:47:00 -0400

    qc_brainmask: check lesion mask

--- preprocessing/qc_brainmask.py
+++ preprocessing/qc_brainmask.py
@@ -11,7 +11,6 @@ parser.add_argument('-s', '--source', type=str, required=True, help="""Input fol
 The folder must follow this structure:
 /input/folder/
  013
-    brain_mask.nii.gz
     flair_time01_on_middle_space.nii.gz
     flair_time02_on_middle_space.nii.gz
     ground_truth_expert1.nii.gz
@@ -20,7 +19,6 @@ The folder must follow this structure:
     ground_truth_expert4.nii.gz
     ground_truth.nii.gz
  015
-    brain_mask.nii.gz
     flair_time01_on_middle_space.nii.gz
     flair_time02_on_middle_space.nii.gz
     ground_truth_expert1.nii.gz
@@ -60,7 +58,7 @@ src_folder = args.source
 dest_folder = args.dest
 
 flairs = ['flair_time01_on_middle_space.nii.gz', 'flair_time02_on_middle_space.nii.gz']
-mask = ['brain_mask.nii.gz']
+mask = 'brain_mask.nii.gz'
 
 for patientName in os.listdir(src_folder):
 
@@ -75,6 +73,7 @@ for patientName in os.listdir(src_folder):
     for flairName in flairs:
         src_flair = os.path.join(src_patient_folder, flairName)
         mask_patient = os.path.join(dest_patient_folder, mask)
+
         # Output fname
         output_fname = os.path.join(dest_patient_folder, 'difference_'+flairName.split('.nii.gz')[0]+'.nii.gz')
 
@@ -83,8 +82,16 @@ for patientName in os.listdir(src_folder):
         mask_im = nib.load(mask_patient)
 
         # Compute difference between the raw data and the brain mask
-        diff_np = np.copy(src_im.data)
-        diff_np[np.where(mask_im)] = 0
+        diff_np = np.copy(src_im.get_fdata())
+        diff_np[np.where(mask_im.get_fdata())] = 0
+
+        # Check whether some new lesion-voxels are affected by brain extraction
+        for expert_id in range(1, 5):
+            gt_fname = os.path.join(src_patient_folder, "ground_truth_expert{}.nii.gz".format(expert_id))
+            gt_im = nib.load(gt_fname)
+            is_gt_cropped = np.any(diff_np * gt_im.get_fdata())
+            if is_gt_cropped:
+                print("\tALERT: Lesion(s) from expert {} cropped during brain extraction.".format(expert_id))
 
         # Save difference
         diff_nib = nib.Nifti1Image(dataobj=diff_np, affine=None, header=src_im.header.copy())
