0d618bfef7bde2e49bb8c0c5a42d76cf6c000804
Uzay Macar <oum2000@columbia.edu>
2021-06-17 03:06:03 +0300

    Implemented binary accuracy for CLF task, and removed  for more efficient memory-usage.

--- modeling/utils.py
+++ modeling/utils.py
@@ -1,6 +1,6 @@
-from copy import deepcopy
-from torch.utils.data import Subset
 from sklearn.model_selection import train_test_split
+import torch
+from torch.utils.data import Subset
 
 
 def split_dataset(dataset, val_size, seed):
@@ -8,14 +8,24 @@ def split_dataset(dataset, val_size, seed):
                                           test_size=val_size,
                                           random_state=seed)
 
-    # Copy the dataset into a new object, and set train mode for the old object
-    # NOTE: This is one "hack" to make sure val dataset doesn't have training augmentations
-    dataset_ = deepcopy(dataset)
-    dataset.set_train_mode()
-
-    train_dataset, val_dataset = Subset(dataset, train_idx), Subset(dataset_, val_idx)
+    train_dataset, val_dataset = Subset(dataset, train_idx), Subset(dataset, val_idx)
 
     print('Divided base dataset of size %d into %d [1] and %d [2] sub-datasets!' %
           (len(dataset), len(train_dataset), len(val_dataset)))
 
     return train_dataset, val_dataset
+
+
+def binary_accuracy(y_pred, y_true):
+    """Function to calculate binary accuracy per batch"""
+    y_pred = y_pred.view(-1, 2)
+    y_true = y_true.view(-1)
+
+    y_pred_max = torch.argmax(y_pred, dim=-1)
+    correct_pred = (y_pred_max == y_true).float()
+
+    acc = (correct_pred.sum() + 1e-5) / (len(correct_pred) + 1e-5)
+    pos_acc = (correct_pred[y_true == 1].sum() + 1e-5) / (len(correct_pred[y_true == 1]) + 1e-5)
+    neg_acc = (correct_pred[y_true == 0].sum() + 1e-5) / (len(correct_pred[y_true == 0]) + 1e-5)
+
+    return acc, pos_acc, neg_acc
